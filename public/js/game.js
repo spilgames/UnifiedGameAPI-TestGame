a3d52a7d736cf8591bfc452fb3304f7550e85f2a
Stephane P. Pericat <stephane.pericat@spilgames.com>
2014-01-21 13:31:39 +0100

    FIX reffactoring to prepare for GameAPI integration, throw alert if you bet on a number > 36

--- public/js/game.js
+++ public/js/game.js
@@ -1,149 +1,181 @@
-var cash = 2500,
-    betAmount = 50;
+(function(K) {
+    var game,
+        layer,
+        btn,
+        betLabel,
+        rouletteLabel,
+        resultLabel,
+        cashLabel,
+        cash = 2500,
+        betAmount = 50;
 
-var stage = new Kinetic.Stage({
-    container: 'game-container',
-    width: 640,
-    height: 480,
-    fill: '#ccc'
-});
-
-var layer = new Kinetic.Layer();
-
-var button = new Kinetic.Group({
-    x: 260,
-    y: 30,
-    width: 120,
-    height: 30
-});
-
-var rect = new Kinetic.Rect({
-    width: 120,
-    height: 30,
-    fill: 'green',
-    stroke: 'black',
-    strokeWidth: 1,
-    cornerRadius: 5
-});
-
-var buttonLabel = new Kinetic.Text({
-    text: 'Enter your bet',
-    fontSize: 16,
-    fontFamily: 'Calibri',
-    fill: 'white',
-    width: 120,
-    y: 7
-});
-
-var betLabel = new Kinetic.Text({
-    fontSize: 16,
-    fontFamily: 'Calibri',
-    fill: 'white',
-    y: 70,
-    width: 640
-});
-
-var rouletteLabel = new Kinetic.Text({
-    fontSize: 50,
-    fontFamily: 'Calibri',
-    fill: 'white',
-    y: 150,
-    width: 640
-});
-
-var resultLabel = new Kinetic.Text({
-    fontSize: 100,
-    fontFamily: 'Calibri',
-    fill: 'white',
-    y: 300,
-    width: 640
-});
-
-var cashLabel = new Kinetic.Text({
-    text: 'Your cash: $' + cash,
-    fontSize: 20,
-    fontFamily: 'Calibri',
-    fill: 'white',
-    width: 600,
-    y: 30,
-    x: 20
-});
-
-buttonLabel.align('center');
-betLabel.align('center');
-rouletteLabel.align('center');
-resultLabel.align('center');
-cashLabel.align('right');
-
-button.on('mouseover', function() {
-    document.body.style.cursor = 'pointer';
-});
-
-button.on('mouseout', function() {
-    document.body.style.cursor = 'default';
-});
-
-button.on('click', function() {
-    if(cash === 0) {
-        window.alert('You cannot bet, you have no more cash!');
-        return false;
+    function _createStage(containerId, width, height) {
+        return new K.Stage({
+            container: containerId,
+            width: width,
+            height: height
+        });
     }
 
-    betLabel.setText('');
-    rouletteLabel.setText('');
-    resultLabel.setText('');
-    layer.draw();
+    function _createLayer() {
+        return new K.Layer();
+    }
 
-    var bet = window.prompt('Enter the number you want to bet on (between 0 and 36):');
+    function _createBetButton() {
+        var button = new K.Group({
+                x: 260,
+                y: 30,
+                width: 120,
+                height: 30
+            }),
+            rect = new K.Rect({
+                width: 120,
+                height: 30,
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 1,
+                cornerRadius: 5
+            }),
+            buttonLabel = new K.Text({
+                text: 'Enter your bet',
+                fontSize: 16,
+                fontFamily: 'Calibri',
+                fill: 'white',
+                width: 120,
+                y: 7
+            }).align('center');
 
-    if(bet) {
-        betLabel.setText('You are betting on: ' + bet);
-        betLabel.align('center');
-        layer.draw();
+        button.add(rect);
+        button.add(buttonLabel);
 
-        var count = 0,
-            result;
+        return button;
+    }
 
-        var getRandomResult = setInterval(function() {
-            if(count < 20) {
-                result = Math.round(Math.random() * 36);
-                count++;
-                rouletteLabel.setText(result);
-                rouletteLabel.align('center');
-                layer.draw();
-            } else {
-                clearInterval(getRandomResult);
+    function _createBetLabel() {
+        return new K.Text({
+            fontSize: 16,
+            fontFamily: 'Calibri',
+            fill: 'white',
+            y: 70,
+            width: 640
+        }).align('center');
+    }
 
-                var message = '';
-                if(result === parseInt(bet)) {
-                    message = 'You won!';
-                    cash += betAmount;
-                } else {
-                    message = 'You lost :(';
-                    cash -= betAmount;
+    function _createRouletteLabel() {
+        return new K.Text({
+            fontSize: 50,
+            fontFamily: 'Calibri',
+            fill: 'white',
+            y: 150,
+            width: 640
+        }).align('center');
+    }
+
+    function _createResultLabel() {
+        return new K.Text({
+            fontSize: 100,
+            fontFamily: 'Calibri',
+            fill: 'white',
+            y: 300,
+            width: 640
+        }).align('center');
+    }
+
+    function _createCashLabel() {
+        return new K.Text({
+            text: 'Your cash: $' + cash,
+            fontSize: 20,
+            fontFamily: 'Calibri',
+            fill: 'white',
+            width: 600,
+            y: 30,
+            x: 20
+        }).align('right');
+    }
+
+    function initGame(containerId, width, height) {
+        game          = _createStage(containerId, width, height);
+        layer         = _createLayer();
+        btn           = _createBetButton();
+        betLabel      = _createBetLabel();
+        rouletteLabel = _createRouletteLabel();
+        resultLabel   = _createResultLabel();
+        cashLabel     = _createCashLabel();
+
+        btn.on('mouseover', function() {
+            document.body.style.cursor = 'pointer';
+        });
+
+        btn.on('mouseout', function() {
+            document.body.style.cursor = 'default';
+        });
+
+        btn.on('click', function() {
+            if(cash === 0) {
+                window.alert('You cannot bet, you have no more cash!');
+                return false;
+            }
+
+            betLabel.setText('');
+            rouletteLabel.setText('');
+            resultLabel.setText('');
+            layer.draw();
+
+            var bet = window.prompt('Enter the number you want to bet on (between 0 and 36):');
+
+            if(bet) {
+                if(bet > 36) {
+                    window.alert('You can only bet on numbers betwen 0 and 36! You bet on: ' + bet);
+                    return false;
                 }
 
-                resultLabel.setText(message);
-                resultLabel.align('center');
-                cashLabel.setText('Your cash: $' + cash);
-                cashLabel.align('right');
+                betLabel.setText('You are betting on: ' + bet);
+                betLabel.align('center');
                 layer.draw();
+
+                var count = 0,
+                    result;
+
+                var getRandomResult = setInterval(function() {
+                    if(count < 20) {
+                        result = Math.round(Math.random() * 36);
+                        count++;
+                        rouletteLabel.setText(result);
+                        rouletteLabel.align('center');
+                        layer.draw();
+                    } else {
+                        clearInterval(getRandomResult);
+
+                        var message = '';
+                        if(result === parseInt(bet)) {
+                            message = 'You won!';
+                            cash += betAmount;
+                        } else {
+                            message = 'You lost :(';
+                            cash -= betAmount;
+                        }
+
+                        resultLabel.setText(message);
+                        resultLabel.align('center');
+                        cashLabel.setText('Your cash: $' + cash);
+                        cashLabel.align('right');
+                        layer.draw();
+                    }
+                }, 100);
             }
-        }, 100);
+        });
 
-        
+        // add the elements to the layer
+        layer.add(cashLabel);
+        layer.add(btn);
+        layer.add(betLabel);
+        layer.add(rouletteLabel);
+        layer.add(resultLabel);
+
+        // add the layer to the game
+        game.add(layer);
     }
-});
 
-button.add(rect);
-button.add(buttonLabel);
+    initGame('game-container', 640, 480);
 
-// add the shape to the layer
-layer.add(cashLabel);
-layer.add(button);
-layer.add(betLabel);
-layer.add(rouletteLabel);
-layer.add(resultLabel);
-
-// add the layer to the stage
-stage.add(layer);
+})(Kinetic);
